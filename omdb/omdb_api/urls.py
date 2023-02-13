1ea86f824ba821c2ab5037ab409646b146f96daf
Andrii <bezkrovnyyav@gmail.com>
2023-02-13 18:38:36 +0200

    Add api token

--- omdb/omdb_api/urls.py
+++ omdb/omdb_api/urls.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView,
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
+    
+    # jwt token 
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
 
 if bool(settings.DEBUG):
