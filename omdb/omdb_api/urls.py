1caaa2572e016b5e7f5962b2bc1170e3fa3cbb60
Andrii <bezkrovnyyav@gmail.com>
2023-02-13 21:16:50 +0200

    Add urls api token

--- omdb/omdb_api/urls.py
+++ omdb/omdb_api/urls.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework_simplejwt.views import TokenObtainPairView,
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+    TokenBlacklistView,
+    TokenObtainPairView,
+    TokenVerifyView,
+)
 
 
 urlpatterns = [
@@ -12,6 +17,9 @@ urlpatterns = [
     
     # jwt token 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
 ]
 
 if bool(settings.DEBUG):
